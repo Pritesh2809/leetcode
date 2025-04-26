12e58a33f984e390636733121600b271587af573
Pritesh2809 <rspritesh009@gmail.com>
2025-04-26 10:48:50 +0530

    Time: 2 ms (58.11%) | Memory: 63.4 MB (89.19%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,12 @@
+var rotate = function(nums, k) {
+    let len = nums.length;
+    k = k % len;
+
+    if (k === 0) return;
+
+    nums.reverse();
+
+    nums.splice(0, k, ...nums.slice(0, k).reverse());
+
+    nums.splice(k, len-k, ...nums.slice(k).reverse());
+};
