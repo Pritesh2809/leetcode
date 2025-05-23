2a692fd80365b3738236faf66c3acf61331a9c8a
Pritesh2809 <rspritesh009@gmail.com>
2025-05-23 12:14:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.67%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -3,14 +3,14 @@ public:
     bool areAlmostEqual(string s1, string s2) {
         if (s1 == s2) return true;
 
-        int first = -1, second = -1, count = 0;
+        int first = 0, second = 0, count = 0;
 
         for (int i = 0; i < s1.length(); i++) {
             if (s1[i] != s2[i]) {
                 count++;
                 if (count > 2) return false; 
 
-                if (first == -1) first = i;
+                if (first == 0) first = i;
                 else second = i;
             }
         }
